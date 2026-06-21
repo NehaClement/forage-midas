ebb7698af97a7778644dd70c159ba90caf82d052
NehaClement <nehaclement91@gmail.com>
2026-06-21 21:55:08 +0530

    Completed Task 1 setup

--- src/main/java/com/jpmc/midascore/component/DatabaseConduit.java
+++ src/main/java/com/jpmc/midascore/component/DatabaseConduit.java
@@ -16,4 +16,8 @@ public class DatabaseConduit {
         userRepository.save(userRecord);
     }
 
+    public UserRecord findById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
 }
